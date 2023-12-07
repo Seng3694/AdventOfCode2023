9d697e59d31d69d93888b3b4c2b7e8310aa660ae
Seng <engelhardt_stefan@gmx.de>
2023-12-07 19:25:23 +0100

    solved day 7 part 2

--- day07/main.c
+++ day07/main.c
@@ -4,7 +4,7 @@
 #include <aoc/aoc.h>
 
 typedef enum {
-  CARD_TYPE_2 = 2,
+  CARD_TYPE_2,
   CARD_TYPE_3,
   CARD_TYPE_4,
   CARD_TYPE_5,
@@ -19,6 +19,22 @@ typedef enum {
   CARD_TYPE_A,
   CARD_TYPE_COUNT,
 } card_type;
+
+static const u8 card_values_with_jack[] = {
+    [CARD_TYPE_2] = 2,  [CARD_TYPE_3] = 3,  [CARD_TYPE_4] = 4,
+    [CARD_TYPE_5] = 5,  [CARD_TYPE_6] = 6,  [CARD_TYPE_7] = 7,
+    [CARD_TYPE_8] = 8,  [CARD_TYPE_9] = 9,  [CARD_TYPE_T] = 10,
+    [CARD_TYPE_J] = 11, [CARD_TYPE_Q] = 12, [CARD_TYPE_K] = 13,
+    [CARD_TYPE_A] = 14,
+};
+
+static const u8 card_values_with_joker[] = {
+    [CARD_TYPE_2] = 2,  [CARD_TYPE_3] = 3,  [CARD_TYPE_4] = 4,
+    [CARD_TYPE_5] = 5,  [CARD_TYPE_6] = 6,  [CARD_TYPE_7] = 7,
+    [CARD_TYPE_8] = 8,  [CARD_TYPE_9] = 9,  [CARD_TYPE_T] = 10,
+    [CARD_TYPE_J] = 1,  [CARD_TYPE_Q] = 11, [CARD_TYPE_K] = 12,
+    [CARD_TYPE_A] = 13,
+};
 
 typedef enum {
   HAND_KIND_HIGH_CARD,
@@ -39,7 +55,7 @@ typedef struct __attribute__((packed)) {
 #define AOC_T card_hand
 #include <aoc/vector.h>
 
-static hand_kind get_hand_kind(const u8 cards[const 5]) {
+static hand_kind get_hand_kind_with_jacks(const u8 cards[const 5]) {
   u8 counts[CARD_TYPE_COUNT] = {0};
   for (u8 i = 0; i < 5; ++i)
     counts[cards[i]]++;
@@ -77,6 +93,70 @@ static hand_kind get_hand_kind(const u8 cards[const 5]) {
   }
 }
 
+static int compare_counts(const void *a, const void *b) {
+  return ((i8) * (u8 *)b) - ((i8) * (u8 *)a);
+}
+
+static hand_kind get_hand_kind_with_jokers(const u8 cards[const 5]) {
+  u8 counts[CARD_TYPE_COUNT] = {0};
+  for (u8 i = 0; i < 5; ++i)
+    counts[cards[i]]++;
+
+  u8 jokers = counts[CARD_TYPE_J];
+  if (jokers == 5)
+    return HAND_KIND_FIVE_OF_A_KIND;
+  counts[CARD_TYPE_J] = 0;
+
+  qsort(counts, CARD_TYPE_COUNT, sizeof(u8), compare_counts);
+
+  bool has5 = false, has4 = false, has3 = false;
+  u8 pairCount = 0;
+  for (u8 i = 0; i < CARD_TYPE_COUNT; ++i) {
+    if (counts[i] == 0)
+      continue;
+    if (counts[i] + jokers == 5) {
+      counts[i] += jokers;
+      has5 = true;
+      jokers = 0;
+      break;
+    } else if (counts[i] + jokers == 4) {
+      counts[i] += jokers;
+      has4 = true;
+      jokers = 0;
+      break;
+    } else if (counts[i] + jokers == 3) {
+      counts[i] += jokers;
+      has3 = true;
+      jokers = 0;
+      break;
+    }
+  }
+
+  for (u8 i = 0; i < CARD_TYPE_COUNT; ++i) {
+    if (counts[i] + jokers == 2) {
+      counts[i] += jokers;
+      pairCount++;
+      jokers = 0;
+    }
+  }
+
+  if (has5) {
+    return HAND_KIND_FIVE_OF_A_KIND;
+  } else if (has4) {
+    return HAND_KIND_FOUR_OF_A_KIND;
+  } else if (has3 && pairCount == 1) {
+    return HAND_KIND_FULL_HOUSE;
+  } else if (has3) {
+    return HAND_KIND_THREE_OF_A_KIND;
+  } else if (pairCount == 2) {
+    return HAND_KIND_TWO_PAIR;
+  } else if (pairCount == 1) {
+    return HAND_KIND_ONE_PAIR;
+  } else {
+    return HAND_KIND_HIGH_CARD;
+  }
+}
+
 static void parse(char *line, aoc_vector_card_hand *const hands) {
   card_hand hand = {0};
   for (u8 i = 0; i < 5; ++i) {
@@ -87,27 +167,37 @@ static void parse(char *line, aoc_vector_card_hand *const hands) {
     case 'Q': hand.cards[i] = CARD_TYPE_Q;   break;
     case 'K': hand.cards[i] = CARD_TYPE_K;   break;
     case 'A': hand.cards[i] = CARD_TYPE_A;   break;
-    default:  hand.cards[i] = line[i] - '0'; break;
+    default:  hand.cards[i] = line[i] - '0' - 2; break;
     }
     // clang-format on
   }
   hand.bid = strtoul(line + 6, NULL, 10);
-  hand.kind = get_hand_kind(hand.cards);
+  hand.kind = get_hand_kind_with_jacks(hand.cards);
   aoc_vector_card_hand_push(hands, hand);
 }
 
-static i32 compare_hand(const card_hand *const a, const card_hand *const b) {
+static i32 compare_hand_with_jokers(const card_hand *const a,
+                                    const card_hand *const b) {
   i32 diff = (i32)a->kind - (i32)b->kind;
   for (u8 i = 0; i < 5 && diff == 0; ++i) {
-    diff = (i32)a->cards[i] - (i32)b->cards[i];
+    diff = (i32)card_values_with_joker[a->cards[i]] -
+           (i32)card_values_with_joker[b->cards[i]];
   }
   return diff;
 }
 
-static u32 solve_part1(const aoc_vector_card_hand *const hands) {
+static i32 compare_hand_with_jacks(const card_hand *const a,
+                                   const card_hand *const b) {
+  i32 diff = (i32)a->kind - (i32)b->kind;
+  for (u8 i = 0; i < 5 && diff == 0; ++i) {
+    diff = (i32)card_values_with_jack[a->cards[i]] -
+           (i32)card_values_with_jack[b->cards[i]];
+  }
+  return diff;
+}
+
+static u32 solve(const aoc_vector_card_hand *const hands) {
   u32 solution = 0;
-  qsort(hands->items, hands->length, sizeof(card_hand),
-        (int (*)(const void *, const void *))compare_hand);
   for (size_t i = 0; i < hands->length; ++i)
     solution += ((i + 1) * hands->items[i].bid);
   return solution;
@@ -119,9 +209,17 @@ int main(void) {
 
   aoc_file_read_lines1("day07/input.txt", (aoc_line_func)parse, &hands);
 
-  const u32 part1 = solve_part1(&hands);
+  qsort(hands.items, hands.length, sizeof(card_hand),
+        (int (*)(const void *, const void *))compare_hand_with_jacks);
+  const u32 part1 = solve(&hands);
 
-  printf("%u\n", part1);
+  for (size_t i = 0; i < hands.length; ++i)
+    hands.items[i].kind = get_hand_kind_with_jokers(hands.items[i].cards);
+  qsort(hands.items, hands.length, sizeof(card_hand),
+        (int (*)(const void *, const void *))compare_hand_with_jokers);
+  const u32 part2 = solve(&hands);
+
+  printf("%u\n%u\n", part1, part2);
 
   aoc_vector_card_hand_destroy(&hands);
 }
