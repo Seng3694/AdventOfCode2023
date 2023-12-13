0075db81aa6948ce296cac0f8e1d29e5b1c30efc
Seng <engelhardt_stefan@gmx.de>
2023-12-13 17:06:04 +0100

    solved day 12 part 2

--- day12/main.c
+++ day12/main.c
@@ -1,15 +1,13 @@
 #include <aoc/aoc.h>
-#include <aoc/common.h>
 #include <aoc/mem.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 typedef struct {
-  char data[32 * 5];
-  u8 counts[6 * 5];
-  u8 dataLength;
-  u8 countsLength;
-  u8 wildcardCount;
+  char state[32 * 5];
+  u8 groups[6 * 5];
+  u8 stateLength;
+  u8 groupsLength;
 } record;
 
 #define AOC_T record
@@ -18,95 +16,105 @@ typedef struct {
 static void parse(char *line, aoc_vector_record *const records) {
   char *current = line;
   record r = {0};
-  while (*current != ' ') {
-    r.wildcardCount += (*current == '?');
+  while (*current != ' ')
     current++;
-  }
-  r.dataLength = current - line;
-  aoc_mem_copy(r.data, line, r.dataLength);
+  r.stateLength = current - line;
+  aoc_mem_copy(r.state, line, r.stateLength);
 
   while (*current != '\n') {
     current++;
-    r.counts[r.countsLength++] = strtoul(current, &current, 10);
+    r.groups[r.groupsLength++] = strtoul(current, &current, 10);
   }
   aoc_vector_record_push(records, r);
 }
 
-static bool is_valid_arrangement(const record *const r) {
-  u8 current = 0;
-  for (u8 i = 0; i < r->dataLength; ++i)
-    if (r->data[i] == '?')
-      return false;
-  u8 found = 0;
-  for (u8 i = 0; i < r->countsLength && current < r->dataLength; ++i) {
-    while (r->data[current] != '#') {
-      current++;
-      if (current >= r->dataLength)
-        return false;
-    }
-    u8 count = 0;
-    for (u8 j = 0; j < r->counts[i] && (current + j) < r->dataLength; ++j) {
-      count++;
-      if (r->data[current + j] != '#')
-        return false;
-    }
-    current += r->counts[i];
-    found += (count == r->counts[i]);
-    if (r->data[current] == '#')
-      return false;
-  }
-  if (found != r->countsLength)
-    return false;
+typedef struct {
+  u32 statePos, groupPos, matched;
+} tuple;
 
-  for (u8 i = current; i < r->dataLength; ++i)
-    if (r->data[i] == '#')
-      return false;
-
-  return true;
+static inline u32 tuple_hash(const tuple *const t) {
+  u32 hash = 59238281;
+  hash = (hash << 5) + t->statePos;
+  hash = (hash << 5) + t->groupPos;
+  hash = (hash << 5) + t->matched;
+  return hash;
 }
 
-static u32 count_arrangements(record r, const u8 start,
-                              const u8 wildcardsLeft) {
-  u32 count = 0;
-  if (wildcardsLeft == 0)
-    count = is_valid_arrangement(&r);
-
-  for (u8 i = start; i < r.dataLength; ++i) {
-    if (r.data[i] == '?') {
-      r.data[i] = '#';
-      count += count_arrangements(r, i + 1, wildcardsLeft - 1);
-      r.data[i] = '.';
-      count += count_arrangements(r, i + 1, wildcardsLeft - 1);
-      break;
-    }
-  }
-
-  return count;
+static inline bool tuple_equals(const tuple *const a, const tuple *const b) {
+  return a->groupPos == b->groupPos && a->statePos == b->statePos &&
+         a->matched == b->matched;
 }
 
-static u32 solve(aoc_vector_record *const records) {
-  u32 sum = 0;
-  for (size_t i = 0; i < records->length; ++i)
-    sum += count_arrangements(records->items[i], 0,
-                              records->items[i].wildcardCount);
+#define AOC_KEY_T tuple
+#define AOC_VALUE_T u64
+#define AOC_KEY_T_EMPTY ((tuple){UINT32_MAX, UINT32_MAX, UINT32_MAX})
+#define AOC_KEY_T_HASH tuple_hash
+#define AOC_KEY_T_EQUALS tuple_equals
+#define AOC_BASE2_CAPACITY
+#include <aoc/map.h>
+
+static aoc_map_tuple_u64 cache = {0};
+
+static u64 count(const record *const r, const u32 statePos, const u32 groupPos,
+                 const u32 matched);
+
+static inline u64 count_inner(const record *const r, const u32 statePos,
+                              const u32 groupPos, const u32 matched) {
+#define DOT()                                                                  \
+  (matched == 0 ? count(r, statePos + 1, groupPos, 0)                          \
+   : (groupPos < r->groupsLength && r->groups[groupPos] == matched)            \
+       ? count(r, statePos + 1, groupPos + 1, 0)                               \
+       : 0)
+
+#define POUND() (count(r, statePos + 1, groupPos, matched + 1))
+  if (statePos == r->stateLength)
+    return (groupPos == r->groupsLength && matched == 0) ||
+           (groupPos == r->groupsLength - 1u &&
+            matched == r->groups[r->groupsLength - 1]);
+
+  switch (r->state[statePos]) {
+  case '#':
+    return POUND();
+  case '.':
+    return DOT();
+  default:
+    return DOT() + POUND();
+  }
+}
+
+static u64 count(const record *const r, const u32 statePos, const u32 groupPos,
+                 const u32 matched) {
+  u64 c = 0;
+  const tuple t = {statePos, groupPos, matched};
+  u32 hash = tuple_hash(&t);
+  if (!aoc_map_tuple_u64_contains_pre_hashed(&cache, t, hash)) {
+    c = count_inner(r, statePos, groupPos, matched);
+    aoc_map_tuple_u64_insert_pre_hashed(&cache, t, c, hash);
+  } else {
+    aoc_map_tuple_u64_get_pre_hashed(&cache, t, &c, hash);
+  }
+  return c;
+}
+
+static u64 solve(const aoc_vector_record *const records) {
+  u64 sum = 0;
+  for (size_t i = 0; i < records->length; ++i) {
+    aoc_map_tuple_u64_clear(&cache);
+    sum += count(&records->items[i], 0, 0, 0);
+  }
   return sum;
 }
 
-static void unfold(aoc_vector_record *const dest,
-                   const aoc_vector_record *const src) {
-  for (size_t i = 0; i < src->length; ++i) {
-    record r = {.dataLength = src->items[i].dataLength * 5 + 4,
-                .countsLength = src->items[i].countsLength * 5};
-    for (u8 j = 0; j < 5; ++j) {
-      size_t destIndex = j * (src->items[i].dataLength + 1);
-      aoc_mem_copy(&r.data[destIndex], src->items[i].data,
-                   src->items[i].dataLength);
-      r.data[destIndex + src->items[i].dataLength] = '?';
-
-      aoc_mem_copy(&r.counts[j * (src->items[i].countsLength)],
-                   src->items[i].counts, src->items[i].countsLength);
+static void unfold(aoc_vector_record *const records) {
+  for (size_t i = 0; i < records->length; ++i) {
+    record *const r = &records->items[i];
+    r->state[r->stateLength++] = '?';
+    for (u8 j = 1; j < 5; ++j) {
+      aoc_mem_copy(&r->state[j * r->stateLength], r->state, r->stateLength);
+      aoc_mem_copy(&r->groups[j * r->groupsLength], r->groups, r->groupsLength);
     }
-    aoc_vector_record_push(dest, r);
+    r->stateLength = (r->stateLength * 5) - 1;
+    r->groupsLength *= 5;
   }
 }
 
@@ -115,14 +123,14 @@ int main(void) {
   aoc_vector_record_create(&records, 1000);
   aoc_file_read_lines1("day12/input.txt", (aoc_line_func)parse, &records);
 
-  aoc_vector_record unfolded = {0};
-  aoc_vector_record_create(&unfolded, 1000);
-  unfold(&unfolded, &records);
+  aoc_map_tuple_u64_create(&cache, 1 << 15);
 
-  const u32 part1 = solve(&records);
-  const u32 part2 = solve(&unfolded);
-  printf("%u\n%u\n", part1, part2);
+  u64 part1 = solve(&records);
+  unfold(&records);
+  u64 part2 = solve(&records);
 
-  aoc_vector_record_destroy(&unfolded);
+  printf("%lu\n%lu\n", part1, part2);
+
+  aoc_map_tuple_u64_destroy(&cache);
   aoc_vector_record_destroy(&records);
 }
